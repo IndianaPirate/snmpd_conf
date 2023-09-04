a8278ba9d266679fabd641979c79e3441cf15be1
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-09-04 11:08:34 +0200

    add new func

---
+++ config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo """
+#################################################
+#		SNMP				#
+#					        #
+################################################
+"""
+function get_ipaddress() {
+	ipadd=$(ip a | grep 192.168.1 | tr -s " " | cut -d " " -f3 | cut -d "/" -f1)
+	echo $ipadd > ip.txt
+	export IPADD=$ipadd
+}
+
+function install_snmp_distro_debian() {
+	echo "[+] Update"
+	apt update -y >/dev/null
+	echo "[+] Install SNMP"
+	apt install snmp -y >/dev/null
+	if [ $? == 0 ]; then
+		echo "Installation success"
+	else
+		echo "Installation failed"
+	fi
+}
+function configure_snmp() {
+	mv /etc/snmp/snmpd.conf /etc/snmp/snmp/snmpd.conf.bak | true
+	echo "rocommunity public" >> /etc/snmp/snmpd.conf
+	echo "master  agentx" >> /etc/snmp/snmpd.conf
+	echo "agentaddress 127.0.0.1,$IPADD,[::1]" >> /etc/snmp/snmpd.conf
+	echo "view   systemonly  included   .1.3.6.1.2.1.1" >> /etc/snmp/snmpd.conf
+	echo "view   systemonly  included   .1.3.6.1.2.1.25.1" >> /etc/snmp/snmpd.conf
+	echo "includeDir /etc/snmp/snmpd.conf.d" >> /etc/snmp/snmpd.conf
+}
+get_ipaddress
+install_snmp_distro_debian
+configure_snmp
