fb0a503e59c8144cd7751e78edb36eff87a34fc1
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-09-04 14:36:25 +0200

    refactor

--- config.sh
+++ config.sh
@@ -16,11 +16,8 @@ function install_snmp_distro_debian() {
 	echo "[+] UPDATE"
 	apt update -y >/dev/null
 	echo "[+] INSTALL SNMP"
-	apt install snmp -y >/dev/null
-	if [ $? == 0 ]
+	if ! apt install snmp -y >/dev/null
 	then
-		echo "[+] Installation success"
-	else
 		echo "[-] Installation failed"
 	fi
 }
