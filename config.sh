3961bd78571e2be7e5b27fc53a7f4e171d1dc118
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-09-04 14:24:40 +0200

    correction erreur

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ echo """
 """
 function get_ipaddress() {
 	ipadd=$(ip a | grep 192.168.1 | tr -s " " | cut -d " " -f3 | cut -d "/" -f1)
-	echo $ipadd > ip.txt
+	echo "$ipadd" > ip.txt
 	export IPADD=$ipadd
 }
 
@@ -17,7 +17,7 @@ function install_snmp_distro_debian() {
 	apt update -y >/dev/null
 	echo "[+] INSTALL SNMP"
 	apt install snmp -y >/dev/null
-	if [ $? == 0 ]; then
+	if [ "$? == 0" ]; then
 		echo "[+] Installation success"
 	else
 		echo "[-] Installation failed"
